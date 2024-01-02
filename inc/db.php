7f9abc5a7746253f914d60de49edd20f3209d942
WorldHatesD <40483048+worldhatesd@users.noreply.github.com>
2024-01-01 19:55:07 -0600

    Update db.php

--- inc/db.php
+++ inc/db.php
@@ -1,5 +1,5 @@
 <?php
-$con = new mysqli("205.185.125.123", "admin_bot", "PGBx3AuLv", "admin_bot");
+$con = new mysqli("205.185.125.123", "aat", "aat", "aat");
 if ($con->connect_errno) {
     echo "Failed to connect to MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
 }
